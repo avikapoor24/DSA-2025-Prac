62e2308f8510a06fc99be1106d7cbbd9434e340e
Avi Kapoor <85234265+avikapoor24@users.noreply.github.com>
2025-08-23 02:14:24 +0530

    Create MinimizeTheHeights2.cpp

---
+++ 450DSA/Arrays/MinimizeTheHeights2.cpp
@@ -0,0 +1,13 @@
+/*
+Given an array arr[] denoting heights of n towers and a positive integer k.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+Increase the height of the tower by k
+Decrease the height of the tower by k
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+You can find a slight modification of the problem here.
+Note: It is compulsory to increase or decrease the height by k for each tower. After the operation, the resultant array should not contain any negative integers.
+*/
+//Link, https://www.geeksforgeeks.org/problems/minimize-the-heights3351/1
