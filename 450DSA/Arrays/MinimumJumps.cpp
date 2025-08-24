fe7c5aa8d9301baed5a8073f208bda25105cd5fc
Avi Kapoor <85234265+avikapoor24@users.noreply.github.com>
2025-08-24 19:38:04 +0530

    Create MinimumJumps.cpp

---
+++ 450DSA/Arrays/MinimumJumps.cpp
@@ -0,0 +1,15 @@
+/*
+Minimum Jumps
+Difficulty: MediumAccuracy: 11.91%Submissions: 1MPoints: 4
+You are given an array arr[] of non-negative numbers. Each number tells you the maximum number of steps you can jump forward from that position.
+
+For example:
+
+If arr[i] = 3, you can jump to index i + 1, i + 2, or i + 3 from position i.
+If arr[i] = 0, you cannot jump forward from that position.
+Your task is to find the minimum number of jumps needed to move from the first position in the array to the last position.
+
+Note:  Return -1 if you can't reach the end of the array.
+*/
+// Link, https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
