4784f1a6aa860706891ab27a84c0098b0426d496
Avi Kapoor <85234265+avikapoor24@users.noreply.github.com>
2025-08-28 17:42:37 +0530

    Create mergeWithoutExtraSpace.cpp

---
+++ 450DSA/Arrays/mergeWithoutExtraSpace.cpp
@@ -0,0 +1,6 @@
+/*
+Merge Without Extra Space
+Difficulty: MediumAccuracy: 32.01%Submissions: 311K+Points: 4Average Time: 20m
+Given two sorted arrays a[] and b[] of size n and m respectively, the task is to merge them in sorted order without using any extra space. Modify a[] so that it contains the first n elements and modify b[] so that it contains the last m elements.
+*/
+
